921a35026b1f36eb77fb150ab9ed300103e05a78
youngtaek_hong <youngtaek_hong@dweax.com>
2024-10-02 12:48:36 +0900

    chore(unused): 사용하지 않는 스웨거 서식 제거

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -7,86 +7,10 @@ import {
 
 const router = Router();
 
-/**
- * @swagger
- * /auth/register:
- *   post:
- *     summary: 회원가입 라우터
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               username:
- *                 type: string
- *                 example: "홍길동"
- *               email:
- *                 type: string
- *                 example: "testuser01@test.com"
- *               password:
- *                 type: string
- *                 example: "dweax123!@#"
- *     responses:
- *       201:
- *         description: User registered successfully
- *       400:
- *         description: User already exists
- */
 router.post('/auth/register', registerUser);
 
-/**
- * @swagger
- * /auth/login:
- *   post:
- *     summary: 로그인 라우터
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 example: "testuser01@test.com"
- *               password:
- *                 type: string
- *                 example: "dweax123!@#"
- *     responses:
- *       200:
- *         description: User logged in successfully
- *       400:
- *         description: Invalid credentials
- */
 router.post('/auth/login', loginUser);
 
-/**
- * @swagger
- * /auth/check-id:
- *   post:
- *     summary: 사용자 ID 중복 여부 확인 라우터
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               username:
- *                 type: string
- *                 example: "testuser01"
- *               email:
- *                 type: string
- *                 example: "testuser01@test.com"
- *     responses:
- *       200:
- *         description: User ID exists or not
- */
 router.post('/auth/check-id', checkUserIdExists);
 
 export default router;
